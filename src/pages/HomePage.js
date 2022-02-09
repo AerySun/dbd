025ff4dddd8d70bcf58345b3132d5fc0f2d4985f
AerySun <janinequinten@gmail.com>
2022-02-09 11:08:06 +0100

    inititial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const HomePage = ({ className }) => {
+    return (
+        <div className={className}>
+            <div>ueue</div>
+        </div>
+    )
+}
+
+export default styled(HomePage)`
+`
