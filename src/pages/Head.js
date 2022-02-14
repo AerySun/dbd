25715d67598a6a6aba3f53e91646400a9b324144
AerySun <81433643+AerySun@users.noreply.github.com>
2022-02-14 23:42:43 +0100

    Style Header

--- src/pages/Head.js
+++ src/pages/Head.js
@@ -6,8 +6,10 @@ const Head = ({ className }) => {
     const [count, setCount] = useState(0)
     return(
         <div className={className}>
-            <input type='search' placeholder="Search..." className="Search" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}></input>
-            <button className='count' onClick={() => setCount(count + 1)}>you searched {count} times</button>
+            <div className="container">
+                <input type='search' placeholder="Search..." className="Search" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}></input>
+                <button className='count' onClick={() => setCount(count + 1)}>you searched {count} times</button>
+            </div>
         </div>
         
     )
@@ -17,4 +19,25 @@ const Head = ({ className }) => {
 
 
 export default styled(Head)`
+background-color: #ccedcc;
+
+.count{
+    border: 1px solid black;
+    background-color: white ;
+    border-radius:.33rem ;
+}
+
+.Search{
+    border-color:none ;
+    border-radius: .33rem;
+    outline:none;
+    text-decoration:none ;
+    appearance:none;
+    border: 1px solid black;
+}
+
+.container{
+    text-align: center;
+}
+
 `
